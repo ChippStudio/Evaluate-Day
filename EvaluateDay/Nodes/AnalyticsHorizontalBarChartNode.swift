6a6ca221931c8ea10499fd4af81a0e1b56fcf92a
Konstantin Tsistjakov <ktsistjakov@gmail.com>
2019-01-30 18:34:15 +0200

    horizontal bar chart

---
+++ EvaluateDay/Nodes/AnalyticsHorizontalBarChartNode.swift
@@ -0,0 +1,13 @@
+//
+//  AnalyticsHorizontalBarChartNode.swift
+//  EvaluateDay
+//
+//  Created by Konstantin Tsistjakov on 30/01/2019.
+//  Copyright © 2019 Konstantin Tsistjakov. All rights reserved.
+//
+
+import UIKit
+
+class AnalyticsHorizontalBarChartNode: ASCellNode {
+
+}
